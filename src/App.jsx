56fc83e88a62c6749c285b28a82d1e95d1c7fc6b
Linggar <landaru@student.unimelb.edu.au>
2023-03-09 17:31:05 +1100

    Fixed Router problem

--- src/App.jsx
+++ src/App.jsx
@@ -23,16 +23,14 @@ const App = () => (
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Hero />} />
-            <Route path="/home" element={<Hero />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/experiences" element={<Experiences />} />
-            <Route path="/aboutMe" element={<AboutMe />} />
-            <Route path="/contactMe" element={<ContactMe />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/home" element={<Hero />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experiences" element={<Experiences />} />
+          <Route path="/aboutMe" element={<AboutMe />} />
+          <Route path="/contactMe" element={<ContactMe />} />
+        </Routes>
       </div>
     </div>
 
