06078cdc0acfbc4baabb5a50ab9ffa238dd7eb2f
Linggar <landaru@student.unimelb.edu.au>
2023-03-12 03:11:37 +1100

    YoutubeEmbed v2

--- src/components/YoutubeEmbed.jsx
+++ src/components/YoutubeEmbed.jsx
@@ -5,19 +5,33 @@ import YouTube from 'react-youtube';
 // https://youtu.be/-_pgcFQ0l64
 // https://www.youtube.com/watch?v=-_pgcFQ0l64&list=PLEsfXFp6DpzQbwYDx1zgcKJ4tzyWFaESK
 class YoutubeEmbed extends Component {
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  _onStateChange(event) {
+    console.log(event);
+  }
+
   render() {
+    const { videoId } = this.props;
     const opts = {
-      //   height: '390',
       width: '500',
       playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        host: `http//www.youtube.com`,
+        host: 'https://www.youtube.com',
         autoplay: 0,
         origin: window.location.origin,
       },
     };
-    const { videoId } = this.props;
-    return <YouTube videoId={videoId} opts={opts} onReady={this.onReady} />;
+    return (
+      <YouTube
+        videoId={videoId}
+        opts={opts}
+        onReady={this._onReady}
+        onStateChange={this._onStateChange}
+      />
+    );
   }
 }
 
